2e0b803fe1fa2277fc512bab2859f00f49954d02
nag <enageswararao@gmail.com>
2018-03-25 02:10:46 +0530

    Nagesh3

--- src/main/java/com/example/pages/LandingPage.java
+++ src/main/java/com/example/pages/LandingPage.java
@@ -17,6 +17,7 @@ public ProfilePage gotoProfilePage(){
 	ProfilePage profpage=new ProfilePage();
 	System,out.println("Lading page added new line")
 	System,out.println("Lading page added 2nd line")
+	System,out.println("Lading page added 3nd line")
 	return profpage;
 }
  
